f9e50faa6671a65918cbc7dff6f4a7c6909004fe
Zeronos <boss16805@gmail.com>
2021-12-03 16:19:22 +0700

    lab8_2 completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,5 +1,41 @@
 //Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
 
+
+
+int main(){
+string name,id,movie,day,quote;
+cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+cout << "?????: ";
+getline(cin,name);
+cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
+cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+cout << name << ": ";
+cin >> id;
+cin.ignore();
+
+string str = "";
+str += id[0];
+str += id[1];
+int gear = stoi(str) - 12;
+
+cout << "Fahsai: I think you may be GEAR " << gear << ". I have a free movie ticket for you.\n";
+cout << "Fahsai: Let's go to the cinema together!!!\n";
+cout << "Fahsai: What movie do you want to watch?\n";
+cout << name << ": ";
+getline(cin,movie);
+cout << "Fahsai: So....which day are you free to go with me?\n";
+cout << name << ": ";
+getline(cin,day);
+cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie <<" with you.\n";
+cout << name << ": ";
+getline(cin,quote);
+cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+return 0;
+
+}
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
 ?????: Luke Skywalker
