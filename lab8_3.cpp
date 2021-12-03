5bb1c6546c42742ba543d5813855a064ff289ef4
Zeronos <boss16805@gmail.com>
2021-12-03 18:09:38 +0700

    lab8_3 completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,8 +1,34 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 char before(char x){
 	//Write your function definition here
+	string num ="0123456789";
+	string albet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char k = '0';
+	int i = 0;
+	while (i < 27)
+	{   
+	    if(toupper(x) == albet[i]){
+			if (x == albet[i]){
+			 if (i != 0){
+				return albet[i-1];
+			}
+			 if (i == 0){
+				return albet[25];
+			}
+	        }
+		    else{
+			  return k;
+		    }
+		}
+		if (x == num[i]){
+			return k;
+		}
+    i++;
+	}
+	return 0;
 }
 
 int main(){
